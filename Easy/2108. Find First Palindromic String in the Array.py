93cec0421edfc5f583f5e4fe1758635f08ee75dd
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 19:32:39 +0530

    Create 2108. Find First Palindromic String in the Array.py

---
+++ Easy/2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        s=""
+        for word in words:
+            if word  == "".join(reversed(word)):
+                s = word
+                break  
+        return s        
+
+# Runtime: 124 ms, faster than 37.62% of Python3 online submissions for Find First Palindromic String in the Array.
+# Memory Usage: 13.8 MB, less than 97.19% of Python3 online submissions for Find First Palindromic String in the Array.
