f416855cf1aa90d4cffd2e7d4dbac66c47159829
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:34:33 +0530

    Update and rename Final Value of Variable After Performing Operations to Final Value of Variable After Performing Operations.py

--- Easy/Final Value of Variable After Performing Operations.py
+++ Easy/Final Value of Variable After Performing Operations.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+# https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
 
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
@@ -9,3 +9,6 @@ class Solution:
             if (operations[i].startswith("+") or operations[i].endswith("+")):
                 s = s + 1            
         return s
+
+# Runtime: 75 ms, faster than 48.53% of Python3 online submissions for Final Value of Variable After Performing Operations.
+# Memory Usage: 14 MB, less than 26.26% of Python3 online submissions for Final Value of Variable After Performing Operations.
