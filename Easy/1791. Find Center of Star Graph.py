5c871b23a418841d25ba61f74ecf70015b7d6e57
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:27:43 +0530

    Update and rename 1791. Find Center of Star Graph to 1791. Find Center of Star Graph.py

--- Easy/1791. Find Center of Star Graph.py
+++ Easy/1791. Find Center of Star Graph.py
@@ -1,7 +1,10 @@
-https://leetcode.com/problems/find-center-of-star-graph/submissions/
+# https://leetcode.com/problems/find-center-of-star-graph/submissions/
 
 def findCenter(self, edges: List[List[int]]) -> int:
         if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
             return edges[0][0]
         else:
             return edges[0][1]
+
+# Runtime: 844 ms
+# Memory Usage: 49.7 MB
