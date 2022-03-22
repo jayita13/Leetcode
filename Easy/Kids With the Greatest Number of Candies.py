5e565ecb438cdf0bc2924018aaadfbd03eaf3aea
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:35:20 +0530

    Update and rename Kids With the Greatest Number of Candies to Kids With the Greatest Number of Candies.py

--- Easy/Kids With the Greatest Number of Candies.py
+++ Easy/Kids With the Greatest Number of Candies.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
 def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         maxi = max(candies)
@@ -9,3 +9,6 @@ def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
             else:
                 out.append(False)
         return out  
+
+# Runtime: 62 ms, faster than 36.11% of Python3 online submissions for Kids With the Greatest Number of Candies.
+# Memory Usage: 13.9 MB, less than 71.24% of Python3 online submissions for Kids With the Greatest Number of Candies.
