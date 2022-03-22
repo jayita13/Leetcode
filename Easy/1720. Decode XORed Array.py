d14eae9047f59e05da058b07fe3d1b98685e01f6
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:25:19 +0530

    Update and rename 1720. Decode XORed Array to 1720. Decode XORed Array.py

--- Easy/1720. Decode XORed Array.py
+++ Easy/1720. Decode XORed Array.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/decode-xored-array/
+# https://leetcode.com/problems/decode-xored-array/
 
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
@@ -6,3 +6,6 @@ class Solution:
         for i in encoded:
             ans.append(ans[-1]^i)
         return ans
+
+# Runtime: 327 ms
+# Memory Usage: 15.8 MB    
