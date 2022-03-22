a4e4acf263e30c11c88dd3fe972d7ebc7327c5f7
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:15:47 +0530

    Update 1221. Split a String in Balanced Strings.py

--- Easy/1221. Split a String in Balanced Strings.py
+++ Easy/1221. Split a String in Balanced Strings.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/split-a-string-in-balanced-strings/
+# https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
 def balancedStringSplit(self, s: str) -> int:
         res = []
@@ -12,3 +12,7 @@ def balancedStringSplit(self, s: str) -> int:
             if sums == 0:
                 p+= 1
         return p
+
+# Runtime: 43 ms, faster than 54.82% of Python3 online submissions for Split a String in Balanced Strings.
+# Memory Usage: 13.9 MB, less than 64.64% of Python3 online submissions for Split a String in Balanced Strings.
+
