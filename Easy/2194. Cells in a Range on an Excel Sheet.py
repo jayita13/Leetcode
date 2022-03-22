0253c2baf7426873469534fbd337d962cd6e41a8
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:32:25 +0530

    Update and rename 2194. Cells in a Range on an Excel Sheet to 2194. Cells in a Range on an Excel Sheet.py

--- Easy/2194. Cells in a Range on an Excel Sheet.py
+++ Easy/2194. Cells in a Range on an Excel Sheet.py
@@ -1,5 +1,4 @@
-https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/
-
+# https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/
 
 def cellsInRange(self, s: str) -> List[str]:
         row1 = int(s[1])
@@ -11,3 +10,6 @@ def cellsInRange(self, s: str) -> List[str]:
             for j in range(row1, row2+1):
                 res.append(f"{chr(i)}{j}") 
         return res
+
+# Runtime: 70 ms, faster than 28.95% of Python3 online submissions for Cells in a Range on an Excel Sheet.
+# Memory Usage: 13.9 MB, less than 72.63% of Python3 online submissions for Cells in a Range on an Excel Sheet.
