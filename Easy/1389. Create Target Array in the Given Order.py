6ae9c42ef315225e810221e1009af23dab994958
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:18:36 +0530

    Update 1389. Create Target Array in the Given Order.py

--- Easy/1389. Create Target Array in the Given Order.py
+++ Easy/1389. Create Target Array in the Given Order.py
@@ -1,8 +1,10 @@
-#https://leetcode.com/problems/create-target-array-in-the-given-order/
-
+# https://leetcode.com/problems/create-target-array-in-the-given-order/
 
 def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
         res = []
         for i in range(0, len(nums)):
             res.insert(index[i], nums[i])
         return res
+
+# Runtime: 36 ms, faster than 86.03% of Python3 online submissions for Create Target Array in the Given Order.
+# Memory Usage: 13.8 MB, less than 70.48% of Python3 online submissions for Create Target Array in the Given Order.
