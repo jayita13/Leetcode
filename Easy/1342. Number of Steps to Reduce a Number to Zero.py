aa07028dc864853ea61cb8e34c678b29cdc221b0
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:11:55 +0530

    Update and rename 1342. Number of Steps to Reduce a Number to Zero to 1342. Number of Steps to Reduce a Number to Zero.py

--- Easy/1342. Number of Steps to Reduce a Number to Zero.py
+++ Easy/1342. Number of Steps to Reduce a Number to Zero.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+#https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 
 
 def numberOfSteps(self, num: int) -> int:
