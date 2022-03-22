4f3ddff4fd67ad4deaebed64296a1342c2872bf7
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:13:08 +0530

    Update 1688. Count of Matches in Tournament.py

--- Easy/1688. Count of Matches in Tournament.py
+++ Easy/1688. Count of Matches in Tournament.py
@@ -1,5 +1,8 @@
-#https://leetcode.com/problems/count-of-matches-in-tournament/
+# https://leetcode.com/problems/count-of-matches-in-tournament/
 
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         return n-1
+   
+# Runtime: 39 ms, faster than 61.98% of Python3 online submissions for Count of Matches in Tournament.
+# Memory Usage: 13.8 MB, less than 62.08% of Python3 online submissions for Count of Matches in Tournament.
