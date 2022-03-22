643f9f045c8f8aef229e8dfadfa20c87e22a6ab3
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-22 12:22:13 +0530

    Update and rename 1588. Sum of All Odd Length Subarrays to 1588. Sum of All Odd Length Subarrays.py

--- Easy/1588. Sum of All Odd Length Subarrays.py
+++ Easy/1588. Sum of All Odd Length Subarrays.py
@@ -1,17 +1,20 @@
-https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+# https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
 
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         s = 0     
-        //checking odd length 
+        # checking odd length 
         # for i in range(len(arr)):
         #     for j in range(i,len(arr)):
         #         if len(arr[i:j+1])%2!=0:
         #             s+= sum(arr[i:j+1])     
         
-        //automatically taking odd length
+        # automatically taking odd length
         for i in range(len(arr)):
             for j in range(i,len(arr),2):
                 s+= sum(arr[i:j+1])
                 
         return s 
+
+# Runtime: 72 ms
+# Memory Usage: 13.9 MB
