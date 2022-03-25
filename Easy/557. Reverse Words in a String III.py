f910ba7f7bd0730b59eb57087f9134f092d4e6a7
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-03-25 11:58:24 +0530

    Create 557. Reverse Words in a String III.py

---
+++ Easy/557. Reverse Words in a String III.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/submissions/
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        new = s.split()
+        s1=[]
+        for i in new:
+            s1.append(i[::-1])
+        
+        return " ".join(s1)
+
+'''Runtime: 57 ms, faster than 56.67% of Python3 online submissions for Reverse Words in a String III.
+Memory Usage: 14.8 MB, less than 20.43% of Python3 online submissions for Reverse Words in a String III.'''
