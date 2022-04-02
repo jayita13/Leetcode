9d18f2fa955b35f7f299276fb8e496496233050a
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-04-02 17:31:37 +0530

    Create 1748. Sum of Unique Elements.py

---
+++ Easy/1748. Sum of Unique Elements.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/sum-of-unique-elements/
+
+def sumOfUnique(self, nums: List[int]) -> int:
+        counts = dict()        
+        for i in nums:
+            counts[i] = counts.get(i, 0) + 1
+        for i,j in counts.copy().items():       // create copy to avoid Runtime error of dictionary changed size during iteration
+            if j > 1:
+                del counts[i]
+        return sum(counts.keys())
+
+''' Runtime: 52 ms, faster than 43.31% of Python3 online submissions for Sum of Unique Elements.
+Memory Usage: 13.9 MB, less than 59.23% of Python3 online submissions for Sum of Unique Elements. '''
