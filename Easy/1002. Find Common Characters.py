f3a784b411273815e77deb0eccf423dc042eabe7
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-05-27 19:09:11 +0530

    Create 1002. Find Common Characters.py

---
+++ Easy/1002. Find Common Characters.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/find-common-characters/
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        seen = list(words[0])
+        for i in words:
+            res = []
+            for j in i:
+                if j in seen:
+                    res.append(j)
+                    seen.remove(j)
+            seen = res
+        return seen
+        
+''' Runtime: 53 ms, faster than 83.76% of Python3 online submissions for Find Common Characters.
+Memory Usage: 13.9 MB, less than 96.19% of Python3 online submissions for Find Common Characters. '''        
