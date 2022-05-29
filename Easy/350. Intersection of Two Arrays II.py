4be4805fdf6c956f8a8d791487a66ef55563fffe
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-05-30 02:02:46 +0530

    Create 350. Intersection of Two Arrays II.py

---
+++ Easy/350. Intersection of Two Arrays II.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = []
+        for i in nums1:
+            if i in nums2:
+                ans.append(i)
+                nums2.remove(i)
+        return ans
