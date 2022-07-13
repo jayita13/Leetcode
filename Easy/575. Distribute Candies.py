ace8a9096b6dd927b2df91720078293a689a293d
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-07-13 14:35:04 +0530

    Create 575. Distribute Candies.py

---
+++ Easy/575. Distribute Candies.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/distribute-candies/
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(candyType)//2, len(set(candyType)))
+        
+     
