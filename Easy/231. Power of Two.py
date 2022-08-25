a584f1bf43366c97a40146b79fe930d70826730c
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-08-25 11:45:20 +0530

    Update 231. Power of Two.py

--- Easy/231. Power of Two.py
+++ Easy/231. Power of Two.py
@@ -2,7 +2,15 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n and not n & n-1
+#         return n and not n & n-1
+
+        if n < 1 :
+            return False        
+
+        while n % 2 == 0 :
+            n /= 2        
+
+        return n == 1
 
 '''Runtime: 28 ms, faster than 97.91% of Python3 online submissions for Power of Two.
 Memory Usage: 13.9 MB, less than 52.61% of Python3 online submissions for Power of Two.'''
