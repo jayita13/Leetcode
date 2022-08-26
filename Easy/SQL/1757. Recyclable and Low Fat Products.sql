6d4da9a9e55c3128015c8896b2e11f3fd4993c4f
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-08-26 11:08:18 +0530

    Update and rename 1757. Recyclable and Low Fat Products.md to 1757. Recyclable and Low Fat Products.sql

---
+++ Easy/SQL/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+Select product_id from Products
+where low_fats = "Y" and recyclable = "Y";
+
+# Runtime: 596 ms, faster than 55.41% of MySQL online submissions for Recyclable and Low Fat Products.
+# Memory Usage: 0B, less than 100.00% of MySQL online submissions for Recyclable and Low Fat Products.
