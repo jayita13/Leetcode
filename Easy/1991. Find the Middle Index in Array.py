35d57020514d4c2b627226cb902481e51a151d51
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-08-28 16:29:27 +0530

    Update 1991. Find the Middle Index in Array.py

--- Easy/1991. Find the Middle Index in Array.py
+++ Easy/1991. Find the Middle Index in Array.py
@@ -12,6 +12,20 @@ class Solution:
             ls += nums[i]
             
         return -1
+    
+#       Alternate but can give TLE
+#         j, k = 0, 1
+        
+#         for i in range(len(nums)):
+#             sl, sr = 0, 0
+#             sl += sum(nums[:j])
+#             j+=1
+#             sr += sum(nums[k:])
+#             k+=1
+#             if sl == sr:
+#                 return i
+
+#         return -1
         
 """ Success Details 
 Runtime: 76 ms, faster than 23.50% of Python3 online submissions for Find the Middle Index in Array.
