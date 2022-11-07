4062e2ed64ab14c97c756d47808885269ecbf64a
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2022-11-07 16:53:21 +0530

    Update 1323. Maximum 69 Number.py

--- Algorithms/Easy/Math/1323. Maximum 69 Number.py
+++ Algorithms/Easy/Math/1323. Maximum 69 Number.py
@@ -2,7 +2,20 @@
 
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        return int(str(num).replace('6','9',1))      
+        return int(str(num).replace('6','9',1))    
+   
+#         Brute-Force solution     
+#         arr = []
+#         for i in range(len(str(num))):
+#             arr.append(num%10)
+#             num//=10
+#         arr = arr[::-1]
+#         for i in range(len(arr)):
+#             if arr[i] == 6:
+#                 arr[i] = 9
+#                 break
+                
+#         return int("".join(map(str, arr)))
 
 # Runtime: 56 ms, faster than 17.35% of Python3 online submissions for Maximum 69 Number.
 # Memory Usage: 13.8 MB, less than 60.06% of Python3 online submissions for Maximum 69 Number.
