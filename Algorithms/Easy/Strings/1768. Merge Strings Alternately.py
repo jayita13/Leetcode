48f6acb2730c8484b2dc17f7af0b8d247a7f18db
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2023-04-18 18:58:27 +0530

    Update 1768. Merge Strings Alternately.py

--- Algorithms/Easy/Strings/1768. Merge Strings Alternately.py
+++ Algorithms/Easy/Strings/1768. Merge Strings Alternately.py
@@ -13,6 +13,25 @@ class Solution:
         merged.append(word2)
 
         return "".join(merged)
+    
+    """
+    s = ""
+        for i,j in zip(w1, w2):
+            s += i
+            s += j
+            w1 = w1.replace(i,"",1)
+            w2 = w2.replace(j,"",1)
+        return s+w1+w2
+    """
 
 ''' Runtime: 32 ms, faster than 88.16% of Python3 online submissions for Merge Strings Alternately.
 Memory Usage: 13.9 MB, less than 19.56% of Python3 online submissions for Merge Strings Alternately. '''
+
+""" Runtime
+24 ms
+Beats
+96.72%
+Memory
+13.8 MB
+Beats
+55.13% """
