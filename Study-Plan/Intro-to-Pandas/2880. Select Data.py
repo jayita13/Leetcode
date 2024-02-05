9d1e799d764a775a0ed9dec9a2ba8a2b3c668386
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 00:41:19 +0530

    Create 2880. Select Data.py

---
+++ Study-Plan/Intro-to-Pandas/2880. Select Data.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/select-data/
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students['student_id'] == 101, ['name', 'age']]
