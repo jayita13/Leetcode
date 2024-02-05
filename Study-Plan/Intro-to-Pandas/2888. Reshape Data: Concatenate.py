b3f0cd7cd04aa8583469cbf9122befae59f77d09
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 01:02:10 +0530

    Create 2888. Reshape Data: Concatenate.py

---
+++ Study-Plan/Intro-to-Pandas/2888. Reshape Data: Concatenate.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/reshape-data-concatenate/
+
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2])
