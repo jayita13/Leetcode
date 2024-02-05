6fffae53389a7918bbca12602b0ee302a0e8455a
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 01:09:53 +0530

    Create 2891. Method Chaining.py

---
+++ Study-Plan/Intro-to-Pandas/2891. Method Chaining.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/method-chaining/
+
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals['weight'] > 100].sort_values(by='weight', ascending=False)[['name']]
