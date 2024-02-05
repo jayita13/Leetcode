b210f8a9e75b4a58c356241fc26829bb2a47ec84
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 00:51:31 +0530

    Create 2884. Modify Columns.py

---
+++ Study-Plan/Intro-to-Pandas/2884. Modify Columns.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/modify-columns/
+
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary'] * 2
+    return employees
