6989cefdb8726dd75a552e05bba8f95a696212bf
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 00:54:20 +0530

    Create 2885. Rename Columns.py

---
+++ Study-Plan/Intro-to-Pandas/2885. Rename Columns.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/rename-columns/
+
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(columns={
+        'id' : 'student_id',
+        'first' : 'first_name',
+        'last' : 'last_name',
+        'age' : 'age_in_years'
+    })
