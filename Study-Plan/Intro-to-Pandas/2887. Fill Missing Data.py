101d751c0bbad21164fdc87fd74ebb08b2df8521
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 01:00:25 +0530

    Create 2887. Fill Missing Data.py

---
+++ Study-Plan/Intro-to-Pandas/2887. Fill Missing Data.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/fill-missing-data/
+
+import pandas as pd
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products['quantity'] = products['quantity'].fillna(0)
+    return products
