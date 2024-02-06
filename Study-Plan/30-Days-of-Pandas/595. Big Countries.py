54d2814522801cac2f0fc98d3d11b8351ec39c1a
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-06 19:56:29 +0530

    Create 595. Big Countries.py

---
+++ Study-Plan/30-Days-of-Pandas/595. Big Countries.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/big-countries/
+
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    return df[['name', 'population', 'area']]
