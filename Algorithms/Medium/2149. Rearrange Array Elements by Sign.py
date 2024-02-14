65bf51235a3167e3311c3f02103aa7a2fc52d1eb
JAYITA BHATTACHARYYA <64038928+jayita13@users.noreply.github.com>
2024-02-14 14:20:58 +0530

    Update 2149. Rearrange Array Elements by Sign.py

--- Algorithms/Medium/2149. Rearrange Array Elements by Sign.py
+++ Algorithms/Medium/2149. Rearrange Array Elements by Sign.py
@@ -14,3 +14,26 @@ class Solution:
             res.append(i)
             res.append(j)
         return res
+
+        # n = len(nums)
+
+        # # Initializing an answer array of size n
+        # ans = [0] * n
+
+        # # Initializing two pointers to track even and
+        # # odd indices for positive and negative integers respectively
+        # pos_index, neg_index = 0, 1
+
+        # for i in range(n):
+        #     if nums[i] > 0:
+        #         # Placing the positive integer at the
+        #         # desired index in ans and incrementing pos_index by 2
+        #         ans[pos_index] = nums[i]
+        #         pos_index += 2
+        #     else:
+        #         # Placing the negative integer at the
+        #         # desired index in ans and incrementing neg_index by 2
+        #         ans[neg_index] = nums[i]
+        #         neg_index += 2
+
+        # return ans
